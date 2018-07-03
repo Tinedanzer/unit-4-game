994eba26847d96f7946cb5fc28b30915d433b023
Tinedanzer <johnhillallen@gmail.com>
2018-07-03 15:32:26 -0500

    one step left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,18 @@ var crystal2= Math.floor(Math.random()*11+1);
 var crystal3= Math.floor(Math.random()*11+1);
 var crystal4= Math.floor(Math.random()*11+1);
 var computerNumber=Math.floor(Math.random()*102+19);
-document.getElementById('what').innerHTML=computerNumber;
+// document.getElementById('what').innerHTML=computerNumber;
+$('#what').text(computerNumber);
 var playerTotal= 0; 
-var wins= 0;
+var victory= 0;
+
 var losses = 0;
 
+// $("#totalscore2").text(crystal1);
 function reset(){
-    Random=Math.floor(Math.random()*101+19);
-    console.log(Random)
-    $('#what').text(Random);
+    computerNumber=Math.floor(Math.random()*101+19);
+    console.log(computerNumber)
+    $('#what').text(computerNumber);
     crystal1= Math.floor(Math.random()*11+1);
     crystal2= Math.floor(Math.random()*11+1);
     crystal3= Math.floor(Math.random()*11+1);
@@ -20,14 +23,55 @@ function reset(){
     playerTotal= 0;
     $('#totalscore2').text(playerTotal);
     } ;
-if (playerTotal===computerNumber) {
-    wins++;
-    $(".wins").html;
+function winner() {   
+    victory++;
+    $("#wins").html;
     reset();
-};
-if(playerTotal>computerNumber){
+    };
+function loser(){
     losses++;
-    $(".losses").html;
+    $("#losses").html;
     reset();
-};
+    };
+
+$('#crystal1').on ('click', function(){
+    playerTotal = playerTotal + crystal1;
+    $('#totalscore2').text(playerTotal);
+    if (playerTotal === computerNumber){
+        winner();
+      }
+      else if (playerTotal > computerNumber){
+        loser();
+      } 
+    })
+$('#crystal2').on ('click', function(){
+        playerTotal = playerTotal + crystal2;
+        $('#totalscore2').text(playerTotal);
+        if (playerTotal === computerNumber){
+            winner();
+          }
+          else if (playerTotal > computerNumber){
+            loser();
+          } 
+    })
+$('#crystal3').on ('click', function(){
+            playerTotal = playerTotal + crystal3;
+            $('#totalscore2').text(playerTotal);
+            if (playerTotal === computerNumber){
+                winner();
+              }
+              else if (playerTotal > computerNumber){
+                loser();
+              } 
+    })
+$('#crystal4').on ('click', function(){
+                playerTotal = playerTotal + crystal4;
+                $('#totalscore2').text(playerTotal);
+                if (playerTotal === computerNumber){
+                    winner();
+                  }
+                  else if (playerTotal > computerNumber){
+                    loser();
+                  } 
+    })
 });
