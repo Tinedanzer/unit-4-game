1db85a00bda7824bf3402a94bf7fa8449969d0b5
Tinedanzer <johnhillallen@gmail.com>
2018-07-03 13:33:42 -0500

    nearly done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,34 @@
-var num1= Math.floor(Math.random()*11+1)
-var num2= Math.floor(Math.random()*11+1)
-var num3= Math.floor(Math.random()*11+1)
-var num4= Math.floor(Math.random()*11+1)
-
+$( document ).ready(function(){
+var crystal1= Math.floor(Math.random()*11+1);
+var crystal2= Math.floor(Math.random()*11+1);
+var crystal3= Math.floor(Math.random()*11+1);
+var crystal4= Math.floor(Math.random()*11+1);
+var computerNumber=Math.floor(Math.random()*102+19);
+document.getElementById('what').innerHTML=computerNumber;
 var playerTotal= 0; 
 var wins= 0;
 var losses = 0;
-var computerNumber=Math.floor(Math.random()*102+19)
 
-if (playerTotal!==computerNumber) {
-    
-} else {
+function reset(){
+    Random=Math.floor(Math.random()*101+19);
+    console.log(Random)
+    $('#what').text(Random);
+    crystal1= Math.floor(Math.random()*11+1);
+    crystal2= Math.floor(Math.random()*11+1);
+    crystal3= Math.floor(Math.random()*11+1);
+    crystal4= Math.floor(Math.random()*11+1);
+    userTotal= 0;
+    $('#totalscore2').text(playerTotal);
+    } 
+
+
+if (playerTotal===computerNumber) {
     wins++;
     $(".wins").html;
+} else {
+    
 }
 $("#wins").html("Wins: " + CrystalGame.wins);
 $("#losses").html("Lost: " + CrystalGame.losses);
+
+});
