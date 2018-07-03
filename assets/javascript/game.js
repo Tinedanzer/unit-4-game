2d6f3828caa339ccbb73c29395ad4f18bf625f9e
Tinedanzer <johnhillallen@gmail.com>
2018-07-02 22:56:54 -0500

    crystal images and preliminary jquery

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+var num1= Math.floor(Math.random()*11+1)
+var num2= Math.floor(Math.random()*11+1)
+var num3= Math.floor(Math.random()*11+1)
+var num4= Math.floor(Math.random()*11+1)
+
+var playerTotal= 0; 
+var wins= 0;
+var losses = 0;
+var computerNumber=Math.floor(Math.random()*102+19)
+
+if (playerTotal!==computerNumber) {
+    
+} else {
+    wins++;
+    $(".wins").html;
+}
+$("#wins").html("Wins: " + CrystalGame.wins);
+$("#losses").html("Lost: " + CrystalGame.losses);
